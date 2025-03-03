1197473ca4d8a506e646b1196fb2cc84fd23c1b9
ErikaGomess <109635107+ErikaGomess@users.noreply.github.com>
2025-03-03 14:47:00 -0300

    feat: add rotas Post, Put e Delete de pacientes

--- routes/pages.php
+++ routes/pages.php
@@ -31,3 +31,23 @@ $obRouter->get('/api/v1/paciente/{id}',[
         }
 ]);
 
+$obRouter->post('/api/v1/paciente',[
+        'middlewares' =>[],
+        function ($request){
+            return new Response(200, Controller\Pacientes::setNewPaciente($request), 'application/json');
+        }
+]);
+
+$obRouter->put('/api/v1/paciente/{id}',[
+        'middlewares' =>[],
+        function ($request, $id){
+            return new Response(200, Controller\Pacientes::setEditPaciente($request, $id), 'application/json');
+        }
+]);
+
+$obRouter->delete('/api/v1/paciente/{id}',[
+        'middlewares' =>[],
+        function ($request, $id){
+            return new Response(200, Controller\Pacientes::setDeletePaciente($request, $id), 'application/json');
+        }
+]);
